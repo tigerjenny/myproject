b481af32ca38b3791c5574d3e4b4ec0e169a2458
Jenny <Jenny@192.168.1.5>
2017-03-13 20:47:15 +0800

    java 功課

---
+++ src/oo/abstraction/Cake.java
@@ -0,0 +1,15 @@
+package oo.abstraction;
+
+public class Cake {
+	String name;
+	int heat;
+	int price;
+	int cost;
+	public  Cake(String name,int heat,int price,int cost) {
+		this.name = name;   //名稱
+		this.heat = heat;   //熱量
+		this.price = price; //售價
+		this.cost = cost;   //成本
+	}
+
+}
---
+++ src/oo/abstraction/CakeTester.java
@@ -0,0 +1,10 @@
+package oo.abstraction;
+
+public class CakeTester {
+
+	public static void main(String[] args) {
+		Cake cake = new Cake("草莓蛋糕",180,45,30);
+		
+	}
+
+}
