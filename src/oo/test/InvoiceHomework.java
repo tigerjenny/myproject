86c4b28f3ae9f08a2d910de60ab3185438e7eae6
鄭家欣 <a0989783233@gmail.com>
2017-04-11 12:55:15 +0800

    兌獎finish

--- src/oo/test/InvoiceHomework.java
+++ src/oo/test/InvoiceHomework.java
@@ -7,40 +7,38 @@ public class InvoiceHomework {
 
 	public static void main(String[] args) {
 		// 本次開獎號碼 331 821 886 554 讓使用者輸入發票號碼（長度不限）判斷尾數3位是否中獎(用Arraylist)
-		/*System.out.println("請輸入發票號碼：");
-		Scanner scanner = new Scanner(System.in);
-		String n = scanner.nextLine();
-		String s = "331";
-		//------------0123456789
-		//String n = "1234567331";
-		System.out.println(n.length());
-		String sub = n.substring(n.length()-3);
-		System.out.println(sub);
-		if(s.equals(sub)){
-			System.out.println("恭喜中獎！！");
-		}else{
-			System.out.println("下次加油～～");
-		}
-		*/
-		
-		
+		/*
+		 * System.out.println("請輸入發票號碼："); Scanner scanner = new
+		 * Scanner(System.in); String n = scanner.nextLine(); String s = "331";
+		 * //------------0123456789 //String n = "1234567331";
+		 * System.out.println(n.length()); String sub =
+		 * n.substring(n.length()-3); System.out.println(sub);
+		 * if(s.equals(sub)){ System.out.println("恭喜中獎！！"); }else{
+		 * System.out.println("下次加油～～"); }
+		 */
+
 		ArrayList<String> a = new ArrayList();
 		a.add("331");
 		a.add("821");
 		a.add("886");
 		a.add("554");
+		boolean flag = false;
 		System.out.println("請輸入發票號碼：");
 		Scanner scanner = new Scanner(System.in);
 		String num = scanner.nextLine();
-		System.out.println(num.length());
-		String sub = num.substring(num.length()-3);
+		String sub = num.substring(num.length() - 3);
 		System.out.println(sub);
-		if(a.equals(sub)){
+		for (int i = 0; i < a.size(); i++) {
+			if (sub.equals(a.get(i))) {
+				flag = true;
+			}
+		}
+
+		if (flag) {
 			System.out.println("恭喜中獎！！");
-		}else{
+		} else {
 			System.out.println("下次加油～～");
 		}
-		
 	}
 
 }
